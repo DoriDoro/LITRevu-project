f86aa2ef3e08c577460afc207870c7f74bdeeb68
doridoro <dorothea.reher@gmail.com>
2023-07-17 18:11:13 +0200

    initial commit - created apps and  models

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    """ Create User instance """
+    pass
+
+
+class UserFollows(models.Model):
+    """  """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name=_("user")
+    )
+    followed_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followed_by',
+        verbose_name=_("follower")
+    )
+
+    class Meta:
+        unique_together = ['user', 'followed_user']
---
+++ review/models.py
@@ -0,0 +1,49 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Ticket(models.Model):
+    """ A ticket is related to a Review. """
+
+    title = models.CharField(max_length=128, verbose_name=_("title of ticket"))
+    description = models.TextField(
+        max_length=2048,
+        verbose_name=_("description of ticket"),
+        blank=True,
+        null=True
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name=_("creator of ticket"),
+    )
+    image = models.ImageField(upload_to='images', verbose_name=_("image"), blank=True, null=True)
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name=_("ticket created at"))
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Review(models.Model):
+    """ A Review has a rating and a ticket. """
+
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, verbose_name=_("ticket"))
+    rating = models.PositiveSmallIntegerField(
+        # validates that rating must be between 0 and 5
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        verbose_name=_("rating")
+    )
+    headline = models.CharField(max_length=128, verbose_name=_("title of review"))
+    body = models.TextField(verbose_name=_("comment"), blank=True, null=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name=_("author of review"),
+    )
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name=_("review created at"))
+
+    def __str__(self):
+        return str(self.headline)
+
