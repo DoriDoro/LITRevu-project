894352ebfde7cd045664f281800a54d1ff9670e9
doridoro <dorothea.reher@gmail.com>
2023-08-11 15:04:31 +0200

    add linebreaks filter to templates. changed rezie to thumbnail in model for image, no change

--- review/models.py
+++ review/models.py
@@ -6,22 +6,23 @@ from PIL import Image
 
 
 class Ticket(models.Model):
-    """ A ticket is related to a Review. """
+    """A ticket is related to a Review."""
 
     title = models.CharField(max_length=128, verbose_name=_("title of ticket"))
     description = models.TextField(
-        max_length=2048,
-        verbose_name=_("description of ticket"),
-        blank=True,
-        null=True
+        max_length=2048, verbose_name=_("description of ticket"), blank=True, null=True
     )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         verbose_name=_("creator of ticket"),
     )
-    image = models.ImageField(upload_to='images', verbose_name=_("image"), blank=True, null=True)
-    time_created = models.DateTimeField(auto_now_add=True, verbose_name=_("ticket created at"))
+    image = models.ImageField(
+        upload_to="images", verbose_name=_("image"), blank=True, null=True
+    )
+    time_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("ticket created at")
+    )
 
     def __str__(self):
         return str(self.title)
@@ -31,23 +32,23 @@ class Ticket(models.Model):
 
         if self.image:
             img = Image.open(self.image.path)
-            img = img.resize((250, 375))
+            img.thumbnail((250, 375))
             img.save(self.image.path)
 
 
 class Review(models.Model):
-    """ A Review has a rating and a ticket. """
+    """A Review has a rating and a ticket."""
 
     ticket = models.ForeignKey(
         Ticket,
         on_delete=models.CASCADE,
         related_name="reviews",
-        verbose_name=_("ticket")
+        verbose_name=_("ticket"),
     )
     rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
         validators=[MinValueValidator(0), MaxValueValidator(5)],
-        verbose_name=_("rating")
+        verbose_name=_("rating"),
     )
     headline = models.CharField(max_length=128, verbose_name=_("title of review"))
     body = models.TextField(verbose_name=_("comment"), blank=True, null=True)
@@ -56,8 +57,9 @@ class Review(models.Model):
         on_delete=models.CASCADE,
         verbose_name=_("author of review"),
     )
-    time_created = models.DateTimeField(auto_now_add=True, verbose_name=_("review created at"))
+    time_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("review created at")
+    )
 
     def __str__(self):
         return f"{self.headline}, {self.ticket}"
-
